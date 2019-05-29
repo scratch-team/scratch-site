320537991f1cff8df0f9501e3656a8d772532ace
martotko <m.georgievv@gmail.com>
2019-05-30 01:08:26 +0300

    Remove unnecessary

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,11 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { HomeRoutingModule } from './home-routing.module';
-import { ButtonStandardModule } from '../shared/button-standard/button-standard.module';
 import { InputDefaultModule } from '../shared/input-default/input-default.module';
 
 @NgModule({
-    imports: [CommonModule, HomeRoutingModule, ButtonStandardModule, InputDefaultModule],
+    imports: [CommonModule, HomeRoutingModule, InputDefaultModule],
     declarations: [HomeComponent],
 })
 export class HomeModule {}
