f1e13013d20866f173d34cdd6c18498837f36370
martotko <m.georgievv@gmail.com>
2019-05-31 19:05:31 +0300

    Add separator

--- src/app/contact/contact.module.ts
+++ src/app/contact/contact.module.ts
@@ -4,9 +4,10 @@ import { ContactComponent } from './contact/contact.component';
 import { ContactRoutingModule } from './contact-routing.module';
 import { InputDefaultModule } from '../shared/input-default/input-default.module';
 import { TextareaModule } from '../shared/textarea/textarea.module';
+import { SeparatorModule } from '../shared/separator/separator.module';
 
 @NgModule({
-    imports: [CommonModule, ContactRoutingModule, InputDefaultModule, TextareaModule],
+    imports: [CommonModule, ContactRoutingModule, InputDefaultModule, TextareaModule, SeparatorModule],
     declarations: [ContactComponent],
 })
 export class ContactModule {}
