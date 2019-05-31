0eab6dd4266781d0a00a3ee00f97a0d9dd88ac12
martotko <m.georgievv@gmail.com>
2019-05-31 19:26:19 +0300

    Add footer

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,13 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { HeaderComponent } from './header/header.component';
-import { RouterModule } from "@angular/router";
+import { RouterModule } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [PageNotFoundComponent, HeaderComponent],
-  imports: [CommonModule, RouterModule ],
-  exports: [PageNotFoundComponent, HeaderComponent]
+    declarations: [PageNotFoundComponent, HeaderComponent, FooterComponent],
+    imports: [CommonModule, RouterModule],
+    exports: [PageNotFoundComponent, HeaderComponent, FooterComponent],
 })
 export class CoreModule {}
