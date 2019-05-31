c2f5d0b1567004bf4f50e5fe065d6b202235c979
martotko <m.georgievv@gmail.com>
2019-05-31 18:51:07 +0300

    Add technologies

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,9 +5,17 @@ import { HomeRoutingModule } from './home-routing.module';
 import { InputDefaultModule } from '../shared/input-default/input-default.module';
 import { SquareIconModule } from '../shared/square-icon/square-icon.module';
 import { MemberPreviewModule } from '../shared/member-preview/member-preview.module';
+import { SeparatorModule } from '../shared/separator/separator.module';
 
 @NgModule({
-    imports: [CommonModule, HomeRoutingModule, InputDefaultModule, SquareIconModule, MemberPreviewModule],
+    imports: [
+        CommonModule,
+        HomeRoutingModule,
+        InputDefaultModule,
+        SquareIconModule,
+        MemberPreviewModule,
+        SeparatorModule,
+    ],
     declarations: [HomeComponent],
 })
 export class HomeModule {}
---
+++ src/app/shared/separator/separator.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SeparatorComponent } from './separator.component';
+
+@NgModule({
+    imports: [CommonModule],
+    declarations: [SeparatorComponent],
+    exports: [SeparatorComponent],
+})
+export class SeparatorModule {}
